b3b1f879bb77264d958a2a0591e5c4acfc60f713
tmurase-master <64181449+tmurase-master@users.noreply.github.com>
2024-01-15 00:07:12 +0900

    Delete trainEye directory

--- trainEye/js/dustbox_JS/init_show_site.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function () {
-    var img = new Image();
-    img.src = './map/site_sample_00001.jpg';
-    img.addEventListener('load', function () {
-        console.log(img.src + "を読み込み完了");
-        siteSet(img);
-        getJsonFileAndSetMarkers("./json/" + 'site_sample_00001.json');
-    });
-})
